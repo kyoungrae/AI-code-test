8ae3ff96123eec7a355f17d5f336eba50a4ccfb5
ikyoungtae <go66go66@naver.com>
2026-01-05 13:44:21 +0900

    init

--- vims-management-system/src/main/java/com/vims/common/siteconfiggroup/CommonSiteConfigGroupService.java
+++ vims-management-system/src/main/java/com/vims/common/siteconfiggroup/CommonSiteConfigGroupService.java
@@ -5,6 +5,9 @@ package com.vims.common.siteconfiggroup;
 
 import com.system.common.base.AbstractCommonService;
 import com.system.common.exception.CustomException;
+import com.vims.common.siteconfig.CommonSiteConfig;
+import com.vims.common.siteconfig.CommonSiteConfigMapper;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -19,32 +22,35 @@ public class CommonSiteConfigGroupService extends AbstractCommonService<CommonSi
     private final CommonSiteConfigGroupMapper commonSiteConfigGroupMapper;
     private final CommonSiteConfigGroupRepository commonSiteConfigGroupRepository;
     private final MessageSource messageSource;
+    private final CommonSiteConfigMapper commonSiteConfigMapper;
 
     private String getMessage(String code) {
         return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
     }
+
     @Override
     protected List<CommonSiteConfigGroup> selectPage(CommonSiteConfigGroup request) throws Exception {
-        try{
+        try {
             return commonSiteConfigGroupMapper.SELECT_PAGE(request);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new CustomException(getMessage(""));
         }
     }
 
     @Override
     protected int selectPagingTotalNumber(CommonSiteConfigGroup request) throws Exception {
-        try{
-          return commonSiteConfigGroupMapper.SELECT_PAGING_TOTAL_NUMBER(request);
-        }catch (Exception e){
+        try {
+            return commonSiteConfigGroupMapper.SELECT_PAGING_TOTAL_NUMBER(request);
+        } catch (Exception e) {
             throw new CustomException(getMessage(""));
         }
     }
+
     @Override
     protected List<CommonSiteConfigGroup> findImpl(CommonSiteConfigGroup request) throws Exception {
-        try{
+        try {
             return commonSiteConfigGroupMapper.SELECT(request);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new CustomException(getMessage(""));
         }
 
@@ -52,29 +58,36 @@ public class CommonSiteConfigGroupService extends AbstractCommonService<CommonSi
 
     @Override
     protected int removeImpl(CommonSiteConfigGroup request) {
-        try{
-            return commonSiteConfigGroupMapper.DELETE(request);
-        }catch (Exception e){
-            throw new CustomException(getMessage(""));
+        List<CommonSiteConfig> list = null;
+        try {
+            var isExitParam = CommonSiteConfig.builder().config_group_id(request.getConfig_group_id()).build();
+            list = commonSiteConfigMapper.SELECT(isExitParam);
+            if (list.isEmpty()) {
+                return commonSiteConfigGroupMapper.DELETE(request);
+            } else {
+                throw new CustomException(getMessage("EXCEPTION.DELETE.EXIST.SBU_DATA"));
+            }
+        } catch (Exception e) {
+            throw e;
         }
     }
 
     @Override
     protected int updateImpl(CommonSiteConfigGroup request) {
-        try{
+        try {
             return commonSiteConfigGroupMapper.UPDATE(request);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new CustomException(getMessage(""));
         }
     }
 
     @Override
-    protected int registerImpl(CommonSiteConfigGroup request){
-        try{
+    protected int registerImpl(CommonSiteConfigGroup request) {
+        try {
             return commonSiteConfigGroupMapper.INSERT(request);
-        }catch (DuplicateKeyException dke){
+        } catch (DuplicateKeyException dke) {
             throw new CustomException(getMessage("EXCEPTION.PK.EXIST"));
-        }catch (Exception e){
+        } catch (Exception e) {
             throw e;
         }
 
