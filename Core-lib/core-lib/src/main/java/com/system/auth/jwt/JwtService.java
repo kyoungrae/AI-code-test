9fd7e9d2a689711580e997b0d86b26f52215fdd0
ikyoungtae <go66go66@naver.com>
2026-01-27 09:23:11 +0900

    init

--- Core-lib/core-lib/src/main/java/com/system/auth/jwt/JwtService.java
+++ Core-lib/core-lib/src/main/java/com/system/auth/jwt/JwtService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@PropertySource("classpath:application.properties")
 @Service
 public class JwtService {
 
