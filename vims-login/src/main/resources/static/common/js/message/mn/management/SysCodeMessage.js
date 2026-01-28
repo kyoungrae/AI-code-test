a7c874cdd96bcc7a70fd14583d884eeefa46a9f8
ikyoungtae <go66go66@naver.com>
2026-01-28 10:41:53 +0900

    몽골어 번역

---
+++ vims-login/src/main/resources/static/common/js/message/mn/management/SysCodeMessage.js
@@ -0,0 +1,24 @@
+//const Message = {};
+//Message.Label = {};
+//Message.Label.Array = {};
+
+Message.Label.Array["SYS_CODE.CHECK.CODE_ID"] = "Дэд кодыг оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.GROUP_ID"] = "Дээд код (Код бүлэг)-ыг оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.CODE_NAME"] = "Дэд кодын нэрийг оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.CODE_NUMBER"] = "Дарааллыг оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.USE_YN"] = "Ашиглах эсэхийг оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.SYSTEM_CREATE_DATE"] = "Үүсгэсэн огноог оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.SYSTEM_CREATE_USERID"] = "Үүсгэсэн хэрэглэгчийн ID-г оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.SYSTEM_UPDATE_DATE"] = "Зассан огноог оруулна уу";
+Message.Label.Array["SYS_CODE.CHECK.SYSTEM_UPDATE_USERID"] = "Зассан хэрэглэгчийн ID-г оруулна уу";
+
+
+Message.Label.Array["SYS_CODE.CODE_ID"] = "Дэд код";
+Message.Label.Array["SYS_CODE.GROUP_ID"] = "Дээд код (Код бүлэг)";
+Message.Label.Array["SYS_CODE.CODE_NAME"] = "Дэд кодын нэр";
+Message.Label.Array["SYS_CODE.CODE_NUMBER"] = "Дараалал";
+Message.Label.Array["SYS_CODE.USE_YN"] = "Ашиглах эсэх";
+Message.Label.Array["SYS_CODE.SYSTEM_CREATE_DATE"] = "Үүсгэсэн огноо";
+Message.Label.Array["SYS_CODE.SYSTEM_CREATE_USERID"] = "Үүсгэсэн хэрэглэгчийн ID";
+Message.Label.Array["SYS_CODE.SYSTEM_UPDATE_DATE"] = "Зассан огноо";
+Message.Label.Array["SYS_CODE.SYSTEM_UPDATE_USERID"] = "Зассан хэрэглэгчийн ID";
---
+++ vims-login/src/main/resources/static/common/js/message/mn/management/SysOfficeMessage.js
@@ -0,0 +1,28 @@
+//const Message = {};
+//Message.Label = {};
+//Message.Label.Array = {};
+Message.Label.Array["SYS_OFFICE.TITLE_DETAIL"] = "Байгууллагын дэлгэрэнгүй";
+Message.Label.Array["SYS_OFFICE.TITLE_LIST"] = "Байгууллагын жагсаалт";
+Message.Label.Array["SYS_OFFICE.TITLE_UPDATE"] = "Байгууллага засах";
+Message.Label.Array["SYS_OFFICE.TITLE_REGISTER"] = "Байгууллага бүртгэх";
+
+Message.Label.Array["SYS_OFFICE.CHECK.TOP_OFFICE_CODE"] = "Хамгийн дээд кодыг оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.OFFICE_NAME"] = "Байгууллагын нэрийг оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.OFFICE_CODE"] = "Байгууллагын кодыг оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.OFFICE_TYPE"] = "Байгууллагын төрлийг оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.OFFICE_TYPE_CODE"] = "Бүртгэлийн газрын ангиллын кодыг оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.SYSTEM_CREATE_USERID"] = "Үүсгэсэн хэрэглэгчийн ID-г оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.SYSTEM_CREATE_DATE"] = "Үүсгэсэн огноог оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.SYSTEM_UPDATE_USERID"] = "Зассан хэрэглэгчийн ID-г оруулна уу";
+Message.Label.Array["SYS_OFFICE.CHECK.SYSTEM_UPDATE_DATE"] = "Зассан огноог оруулна уу";
+
+
+Message.Label.Array["SYS_OFFICE.TOP_OFFICE_CODE"] = "Хамгийн дээд код";
+Message.Label.Array["SYS_OFFICE.OFFICE_NAME"] = "Байгууллагын нэр";
+Message.Label.Array["SYS_OFFICE.OFFICE_CODE"] = "Байгууллагын код";
+Message.Label.Array["SYS_OFFICE.OFFICE_TYPE"] = "Байгууллагын төрөл";
+Message.Label.Array["SYS_OFFICE.OFFICE_TYPE_CODE"] = "Бүртгэлийн газрын ангиллын код";
+Message.Label.Array["SYS_OFFICE.SYSTEM_CREATE_USERID"] = "Үүсгэсэн хэрэглэгчийн ID";
+Message.Label.Array["SYS_OFFICE.SYSTEM_CREATE_DATE"] = "Үүсгэсэн огноо";
+Message.Label.Array["SYS_OFFICE.SYSTEM_UPDATE_USERID"] = "Зассан хэрэглэгчийн ID";
+Message.Label.Array["SYS_OFFICE.SYSTEM_UPDATE_DATE"] = "Зассан огноо";
