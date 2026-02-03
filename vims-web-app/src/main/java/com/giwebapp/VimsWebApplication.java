789366846ec7913f316c58e86372316f81baec61
ikyoungtae <go66go66@naver.com>
2026-02-03 12:32:14 +0900

    init

--- vims-web-app/src/main/java/com/giwebapp/VimsWebApplication.java
+++ vims-web-app/src/main/java/com/giwebapp/VimsWebApplication.java
@@ -3,7 +3,10 @@ package com.giwebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class VimsWebApplication {
 
     public static void main(String[] args) {
